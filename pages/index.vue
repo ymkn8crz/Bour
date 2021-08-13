2ad19041a0903a192337c4d9482e0164eaffea02
ymkn8crz <nofollow0123@gmail.com>
2021-08-14 02:04:51 +0900

    create and update 2 page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,54 @@
 <template>
-  <Tutorial/>
+  <main class="topPage">
+    <div class="topPage__title">
+      <div>page title</div>
+      <div>~Only With You~</div>
+    </div>
+    <div class="topPage__link">
+      <p style="text-align:center;">Directory Contents</p>
+      <ul>
+        <li><a href="/npb/">NPB</a></li>
+        <li><a>NPB TEAM</a></li>
+        <li><a>NPB TEAM</a></li>
+        <li><a>NPB TEAM</a></li>
+        <li><a>NPB TEAM</a></li>
+        <li><a>NPB TEAM</a></li>
+        <li><a>NPB TEAM</a></li>
+      </ul>
+    </div>
+    <div class="topPage__slider">this is slider</div>
+  </main>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<style lang="scss">
+.topPage {
+  &__title {
+    border: solid;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &__link {
+    height: 300px;
+    ul li {
+      display: inline-block;
+      width: 25%;
+    }
+  }
+  &__slider {
+    height: 300px;
+    border: solid;
+  }
+}
+</style>
 
-export default Vue.extend({})
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  head: {
+    title: 'Hello Favorite Directory!!' 
+  }
+})
 </script>
---
+++ pages/npb/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <main class="npbPage">
+    <div>
+      <div class="npbPage__title">page title</div>
+      <div class="npbPage__annotation">
+        <div class="annotation">
+          <dl>
+            <dt @click="isShow = !isShow" style="background-color: red;">大阪とは</dt>
+            <!-- <dt>大阪とは</dt> -->
+            <SlideUpDown :active="isShow" :duration="200">
+              <dd style="background-color: yellow;">セレッソのこと</dd>
+            </SlideUpDown>
+          </dl>
+        </div>
+      </div>
+      <div class="npbPage__slider">
+        <a href="/">topへ</a>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+.npbPage {
+  &__title {
+    border: solid;
+    height: 150px;
+  }
+  &__annotation {
+    height: 200px;
+    width: 80%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    .annotation {
+      height: 100px;
+      margin-top: 20px;
+      background-color: aqua;
+    }
+  }
+  &__slider {
+    background-color: chartreuse;
+    height: 300px;
+  }
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import SlideUpDown from 'vue-slide-up-down'
+
+export default defineComponent({
+  head: {
+    title: 'NPB' 
+  },
+  components:{
+    SlideUpDown,
+  },
+  setup() {
+    const isShow = ref<Boolean>(false)
+    return {
+      isShow,
+    }
+  }
+})
+</script>
